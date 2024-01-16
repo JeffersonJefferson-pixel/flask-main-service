3c5a90852f1a05c09160a29082d76d5c70858aa7
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-01-17 00:59:23 +0700

    add flask main service

---
+++ producer.py
@@ -0,0 +1,12 @@
+import pika, json, os
+
+params = pika.URLParameters(os.environ['RABBITMQ_URL'])
+
+connection = pika.BlockingConnection(params)
+
+
+channel = connection.channel()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
